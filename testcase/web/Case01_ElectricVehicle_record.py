795c8a5407143d5b4d1378920fe75c2b780c1063
uyaly <uyaly@qq.com>
2018-03-27 14:20:48 +0800

    app用例

--- testcase/web/Case01_ElectricVehicle_record.py
+++ testcase/web/Case01_ElectricVehicle_record.py
@@ -35,36 +35,46 @@ class addcompany(unittest.TestCase):
 
     def test02_add(self):
         u'''电动车备案登记，新增'''
-        time.sleep(3)
-        self.driver.find_element("xpath", ".//*[@id='mainlayout_body']/div[1]/div/div[2]/ul/li[2]/a" ).click()   # 电动车监控
+        time.sleep(2)
+        self.driver.find_element("xpath", ".//*[@id='mainlayout_body']/div[1]/div/div[2]/ul/li[2]/a" ).click()   # 横向导航菜单：电动车管理
         ifr = self.driver.find_elements_by_tag_name("iframe")
         self.driver.switch_to.frame(ifr[1])
-        time.sleep(2)
+        time.sleep(1)
         self.driver.find_element("xpath", ".//*[@id='accordion']/div[2]/div[1]/div[1]").click()    # 电动车管理
-        time.sleep(2)
-        self.driver.find_element("xpath", ".//*[@id='accordion']/div[2]/div[2]/ul/li[1]/a").click()    # 电动车备案登记
+        time.sleep(1)
+        self.driver.find_element("xpath", ".//*[@id='accordion']/div[2]/div[2]/ul/li[2]/a").click()    # 电动车备案登记
         ifr1 = self.driver.find_elements_by_tag_name("iframe")
         self.driver.switch_to.frame(ifr1[1])
-        self.driver.find_element("class name", "addBtn").click()
 
-        self.driver.find_element("id", 'telephoneForRegister').send_keys("18062427385") # 手机号
-        # self.driver.find_element("id", 'ilinkappUserName').send_keys(u"张三") # 姓名
-        self.driver.find_element("name", 'chipId').send_keys("65523") # 芯片编号
-        self.driver.find_element("name", 'vehicleOwnerIdcard').send_keys("65523") # 身份证
-        # self.driver.find_elements_by_class_name("validatebox-text")[0].find_element_by_id("_easyui_combobox_i12_0").click()
-        # self.driver.find_elements_by_class_name("validatebox-text")[1].find_element_by_id("_easyui_combobox_i13_0").click()
-        # self.driver.find_elements_by_class_name("validatebox-text")[2].find_element_by_id("_easyui_combobox_i14_0").click()
-        s = self.driver.find_elements_by_class_name("combo-text")
-        for i in range(len(s)):
-            print s[i]
-        s[2].click()
-        self.driver.find_elements_by_id("_easyui_combobox_i12_0")
 
-        # self.driver.find_elements_by_xpath(".//*[@id='_easyui_combobox_i13_0']").click()
-        # self.driver.find_elements_by_xpath(".//*[@id='_easyui_combobox_i14_0']").click()
-
-        # self.driver.find_element("name", 'vehicleIdmunber').send_keys(u"湖北省武汉市洪山区")
+        time.sleep(2)
+        # 新增按钮
+        self.driver.find_element("xpath", ".//*[@id='searchForm']/button[2]").click()
+        time.sleep(2)
+        self.driver.find_element("id", 'telephoneForRegister').send_keys("18062427385")  # 手机号
+        self.driver.find_element("name", 'chipId').send_keys("65523")  # 芯片编号
+        self.driver.find_elements("class name", 'combo-arrow')[5].click()  # 安装点
+        self.driver.find_element("id", '_easyui_combobox_i15_0').click()  # 安装点下拉选第一项
+        self.driver.find_elements("class name", 'combo-arrow')[7].click()  # 区域-市
+        self.driver.find_element("id", '_easyui_combobox_i21_0').click()  # 下拉选第一项
+        self.driver.find_elements("class name", 'combo-arrow')[8].click()  # 区域-区
+        self.driver.find_element("id", '_easyui_combobox_i22_0').click()  # 下拉选第一项
+        self.driver.find_element("name", 'vehicleIdmunber').send_keys(u"鄂L6666")  # 车牌号
+        self.driver.find_element("name", 'frameCode').send_keys("65523")  # 车架号
+        self.driver.find_elements("class name", 'combo-arrow')[9].click()  # 车辆类型
+        self.driver.find_element("id", '_easyui_combobox_i16_0').click()  # 下拉选第一项
+        self.driver.find_elements("class name", 'combo-arrow')[10].click()  # 车辆品牌
+        self.driver.find_element("id", '_easyui_combobox_i18_0').click()  # 下拉选第一项
+        self.driver.find_elements("class name", 'combo-arrow')[12].click()  # 车辆颜色
+        self.driver.find_element("id", '_easyui_combobox_i17_0').click()  # 下拉选第一项
+        self.driver.find_elements("class name", 'combo-arrow')[11].click()  # 购车日期
+        self.driver.find_element("link text", u'今天').click()  # 购车日期-今天
         self.driver.find_element("id", 'saveBtn').click() # 保存
+        # msg = self.driver.find_element("class name", "messager-window")
+        # msg = self.driver.find_element("xpath", "html/body/div[45]/div[2]/div[1]")
+        # print(msg.text)
+        self.driver.find_element("link text", u'确定').click()  # 确定
+
 
         self.driver.switch_to.default_content()
     #     self.psw = Config().get('PASSWORD')
